f0994bc477613819769f8bd874e7e76581f426da
James <metal.lunchbox@gmail.com>
2018-12-30 15:33:41 +0800

    add front-end w/ pug

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+const path = require('path');
+const favicon = require('serve-favicon');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
+const moment = require('moment');
+const marked = require('marked');
 const logger = require('./logger');
+const errors = require('@feathersjs/errors');
 
 const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
@@ -12,9 +17,7 @@ const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
 const channels = require('./channels');
-
 const mongoose = require('./mongoose');
-
 const authentication = require('./authentication');
 
 const app = express(feathers());
@@ -27,7 +30,80 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+// Host the public folder
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', express.static(app.get('public')));
 
+moment.locale('zh-cn')
+
+app.get('/', async (req, res, next) => {
+  try {
+    const groups = await app
+      .service('groups')
+      .find({ query: { $sort: { name: 1 } } });
+    res.render('index', {
+      title: '果园英语首页',
+      groups: groups.data
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/classes/:name', async (req, res, next) => {
+  const { name } = req.params;
+  try {
+    const groupres = await app.service('groups').find({ query: { name } });
+    const group = groupres.data[0];
+    // if no group redirect to an error page
+    if (!group) throw new errors.NotFound('group does not exist');
+    const lessons = await app
+      .service('lessons')
+      .find({ query: { group: group._id, $sort: { number: -1 } } });
+    res.render('class', {
+      title: `果园英语：${name}班课程内容`,
+      lessons: lessons.data,
+      group,
+      moment
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/summary/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const lesson = await app.service('lessons').get(id);
+    const group = await app.service('groups').get(lesson.group);
+
+    const getMaterials = async () => {
+      if (!lesson.materials.length) {
+        return null;
+      }
+      const materials = await app
+        .service('materials')
+        .find({ query: { _id: { $in: lesson.materials } } });
+      if (materials.total > 0) {
+        return materials.data;
+      } else {
+        return null;
+      }
+    };
+    const materials = await getMaterials();
+    res.render('summary', {
+      title: `果园英语：第${lesson.number}课`,
+      lesson,
+      group,
+      materials,
+      moment, marked
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 // Set up Plugins and providers
 app.configure(express.rest());
 app.configure(socketio());
